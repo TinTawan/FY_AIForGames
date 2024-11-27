1a537ed1fc07d05236355adca90b03888d901a74
Justin <jw4566c@gre.ac.uk>
2024-11-27 17:55:51 +0000

    setting up checkneighbours for 2d colliders

--- CellAut/Assets/Scripts/GenerateSpriteGrid.cs
+++ CellAut/Assets/Scripts/GenerateSpriteGrid.cs
@@ -9,14 +9,25 @@ public class GenerateSpriteGrid : MonoBehaviour
  
     RectTransform rectTransform, cellRectTransform;
 
+    SpriteCell[] cells;
+
+    [Tooltip("How often, in seconds, each generation happens when auto generating")]
+    [SerializeField] float genTime = 1f;
+    float timer = 0f;
+    bool isAuto = false;
+
+    [Header("Cell Generation")]
+    [Tooltip("Each generation, a random number of cells will be set to this value\n0 = Deep Blue, 1 = White")]
+    [SerializeField] private float randomCellLevel = 0.8f;
+    [Tooltip("Lower number = higher chance")]
+    [SerializeField] [Range(0, 10)] private int chanceOfRandom = 5;
+    [Tooltip("When a random number of cells are changed each generation, AllCells.Length is divided by this number to choose how many cells will be changed\nLower number = More cells changed")]
+    [SerializeField] [Range(5, 20)] private int cellCountDivider = 10;
 
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
-        cellRectTransform = cellPrefab.GetComponent<RectTransform>();
-
-        //int cellWidth = (int)cellRectTransform.rect.width;
-        //int cellHeight = (int)cellRectTransform.rect.height;
+        //cellRectTransform = cellPrefab.GetComponent<RectTransform>();
 
         for (int i = 1; i < rectTransform.rect.width / cellSize; i++)
         {
@@ -29,8 +40,71 @@ public class GenerateSpriteGrid : MonoBehaviour
                 initCell.GetComponent<RectTransform>().sizeDelta = new(cellSize, cellSize);
                 initCell.GetComponent<BoxCollider2D>().size = new(cellSize, cellSize);
 
-                Debug.Log(i*j);
+                //Debug.Log(i*j);
             }
+        }
+
+        cells = FindObjectsOfType<SpriteCell>();
+    }
+
+    private void Update()
+    {
+        if (isAuto)
+        {
+            AutoGen();
+        }
+    }
+
+
+    public void NextGen()
+    {
+
+        foreach (SpriteCell cell in cells)
+        {
+            cell.NextGeneration();
+
+            /*int cellsToSet = Random.Range(0, 5);
+            if (cellsToSet == 0)
+            {
+                cells[Random.Range(0, cells.Length)].SetCellLevel(0.8f);
+            }*/
+        }
+
+        RandCellLevel(randomCellLevel);
+    }
+
+
+    void AutoGen()
+    {
+        if (timer > genTime)
+        {
+            timer = 0;
+            NextGen();
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    public void ToggleAuto()
+    {
+        isAuto = !isAuto;
+
+    }
+
+    void RandCellLevel(float newCellLevel)
+    {
+        int cellsToSet = Random.Range(0, chanceOfRandom);
+        if (cellsToSet == 0)
+        {
+            int randNumOfCells = Random.Range(0, cells.Length / cellCountDivider);
+            for (int i = 0; i < randNumOfCells; i++)
+            {
+                cells[Random.Range(0, cells.Length)].SetCellLevel(newCellLevel);
+                //Debug.Log("pluh");
+            }
+
         }
     }
 
--- CellAut/Assets/Scripts/SpriteCell.cs
+++ CellAut/Assets/Scripts/SpriteCell.cs
@@ -14,12 +14,15 @@ public class SpriteCell : MonoBehaviour
 
     float subtractVal;
 
-    private List<Cell> neighbourCells = new List<Cell>();
+    private List<SpriteCell> neighbourCells = new List<SpriteCell>();
     //private Cell[] allCells;
+
+    RectTransform rectTransform;
 
     public void Start()
     {
         image = GetComponentInChildren<Image>();
+        rectTransform = GetComponent<RectTransform>();
 
         cellLevel = Random.Range(0f, 1f);
         image.color = SetCellColour(cellLevel);
@@ -29,18 +32,19 @@ public class SpriteCell : MonoBehaviour
 
     void GetNeighbourCells()
     {
-        Collider[] neighbours;
-        neighbours = Physics.OverlapSphere(image.transform.position, checkRadius, cellLayer);
+        Collider2D[] neighbours;
+        //neighbours = Physics.OverlapSphere(image.transform.position, checkRadius, cellLayer);
+        neighbours = Physics2D.OverlapCircleAll(transform.position, checkRadius, cellLayer);
 
-        foreach (Collider col in neighbours)
+        foreach (Collider2D col in neighbours)
         {
             //skip this object so it doesnt include itself
-            if (col == this.GetComponent<Collider>())
+            if (col == this.GetComponent<Collider2D>())
             {
                 continue;
             }
 
-            if (col.gameObject.TryGetComponent(out Cell cell))
+            if (col.gameObject.TryGetComponent(out SpriteCell cell))
             {
                 neighbourCells.Add(cell);
             }
@@ -50,10 +54,65 @@ public class SpriteCell : MonoBehaviour
 
     }
 
+    public void NextGeneration()
+    {
+        GetNeighbourCells();
+
+        //subtract proportion of cells value (0-1.5x)
+        subtractVal = Random.Range(0, cellLevel * 0.5f);
+        cellLevel -= subtractVal;
+
+        float ratio = Random.value;
+        float val1 = subtractVal * ratio;
+        float val2 = subtractVal * (1 - ratio);
+
+        //then get up and right cell
+        // -- for up check x pos is the same and y is >, for right check y is same and x >
+        //Cell upCell, rightCell;
+        foreach (SpriteCell cell in neighbourCells)
+        {
+            RectTransform currentCellRectTransform = cell.GetComponent<RectTransform>();
+            //and add a proportion of the subtract val to each of their cell levels
+            //up cell
+            if (currentCellRectTransform.rect.x == rectTransform.rect.x && currentCellRectTransform.rect.y > rectTransform.rect.y)
+            {
+                /*cell.cellLevel += (val1 > val2) ? val2 : val1;
+
+                cell.cellLevel = Mathf.Clamp(cellLevel, 0f, 1f);*/
+
+                Debug.Log($"{gameObject.name} - up cell: {cell.name}");
+            }
+            //right cell
+            if (currentCellRectTransform.rect.x > rectTransform.rect.x && currentCellRectTransform.rect.y == rectTransform.rect.y)
+            {
+                /*cell.cellLevel += (val1 > val2) ? val1 : val2;
+
+                cell.cellLevel = Mathf.Clamp(cellLevel, 0f, 1f);*/
+
+                Debug.Log($"{gameObject.name} - right cell: {cell.name}");
+            }
+
+            
+        }
+
+        //then randomly choose a number of cells to set to given value;
+        //ChooseRandCellsToSet(0.8f);
+
+        //finally clamp value between 0 and 1 and set colour
+        //cellLevel = Mathf.Clamp(cellLevel, 0, 1);
+        image.color = SetCellColour(cellLevel);
+
+
+    }
+
     Color SetCellColour(float newCellLevel)
     {
         return Color.Lerp(minColour, maxColour, newCellLevel);
 
     }
 
+    public void SetCellLevel(float inLevel)
+    {
+        cellLevel = inLevel;
+    }
 }
