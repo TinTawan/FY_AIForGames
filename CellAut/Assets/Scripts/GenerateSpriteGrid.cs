35af6bfe5e8366a73aaee0395f739420b2658059
Justin <jw4566c@gre.ac.uk>
2024-11-27 16:53:38 +0000

    changed back to other cell sprite and set up grid generation better
    35 seems to be the sweet spot for cell size

--- CellAut/Assets/Scripts/GenerateSpriteGrid.cs
+++ CellAut/Assets/Scripts/GenerateSpriteGrid.cs
@@ -5,24 +5,33 @@ using UnityEngine;
 public class GenerateSpriteGrid : MonoBehaviour
 {
     [SerializeField] private GameObject cellPrefab;
-
-    RectTransform rectTransform;
+    [SerializeField] private float cellSize = 50;
+ 
+    RectTransform rectTransform, cellRectTransform;
 
 
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
+        cellRectTransform = cellPrefab.GetComponent<RectTransform>();
 
-        for (int i = 0; i < rectTransform.rect.width / 50; i++)
+        //int cellWidth = (int)cellRectTransform.rect.width;
+        //int cellHeight = (int)cellRectTransform.rect.height;
+
+        for (int i = 1; i < rectTransform.rect.width / cellSize; i++)
         {
-            for (int j = 0; j < rectTransform.rect.height / 50; j++)
+            for (int j = 1; j < rectTransform.rect.height / cellSize; j++)
             {
-                Vector2 place = new(i * 50, j * 50);
+                Vector2 place = new(i * cellSize, j * cellSize);
                 GameObject initCell = Instantiate(cellPrefab, place, Quaternion.identity, this.transform);
                 initCell.name = $"Cell {i}-{j}";
 
+                initCell.GetComponent<RectTransform>().sizeDelta = new(cellSize, cellSize);
+                initCell.GetComponent<BoxCollider2D>().size = new(cellSize, cellSize);
 
+                Debug.Log(i*j);
             }
         }
     }
+
 }
